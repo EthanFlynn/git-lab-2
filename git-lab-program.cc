4d9955c1223f1802c2c668af0d47ac44afff3ca3
Ethan Flynn <ef040918@ohio.edu>
2019-09-05 16:07:29 -0400

    Hello Gitgit add git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Ethan Flynn
+ *        Date: 9/5/19
+ * Description: "Hello Git!!"
+ */
+
+ #include <iostream>
+ #include <iomanip>
+ #include <cstdlib>
+ using namespace std;
+
+
+ int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}
